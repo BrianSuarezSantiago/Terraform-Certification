bd725529405590c0bc45e7a1eae9456c230fdb68
Brian <108537445+BrianSuarezSantiago@users.noreply.github.com>
2024-05-30 10:07:18 +0200

    Samples

--- Challenges with count meta argument/problems_with_count.tf
+++ Challenges with count meta argument/problems_with_count.tf
@@ -1,7 +1,7 @@
 provider "aws" {}
 
 # To correctly check the problems associated with the use of count.index while referencing
-# try to add a new username at the begginig of the list (variable usernames) and also change
+# try to add a new username at the beginning of the list (variable usernames) and also change
 # the count parameter (under the resource block) and after that, plan or apply it.
 variable "usernames" {
     description = "Contains the names for each user"
